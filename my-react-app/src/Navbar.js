89a8d8f505d42c8c135ed13ef59d6f09726f3501
annie-ye4 <113400571+annie-ye4@users.noreply.github.com>
2024-01-06 13:57:58 -0800

    working on navbar

--- my-react-app/src/Navbar.js
+++ my-react-app/src/Navbar.js
@@ -1,29 +1,22 @@
 // Navbar.js
 
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css'; // Import your Navbar styles
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
-const Navbar = () => {
+function Navbar() {
   return (
     <nav className="navbar">
       <ul>
         <li>
-          {/* Replace "Home" text with the image */}
           <Link to="/">
-            <img src="path/to/home-icon.png" alt="Home" />
+            <img src="my-react-app/src/logo.png" alt="Home" />
           </Link>
         </li>
-        {/* Add other navigation links as needed */}
-        {/* <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li> */}
+        {/* Add more links as needed */}
       </ul>
     </nav>
   );
-};
+}
 
 export default Navbar;
